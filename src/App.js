67502818e9c7eba2d9251ef3f0161d76997b031b
MUKESH KUMAR <66511917+Mukesh16407@users.noreply.github.com>
2022-07-20 19:18:34 +0530

    add component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from './Components/Button';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Button/>
     </div>
   );
 }
